5a482eb947a1beba60213790e73ce0937face0b9
Jonathan Powers <jonathan.powers@revature.net>
2021-08-24 17:10:10 -0400

    Update

---
+++ Project-0/Banking API/com/banking/Simulation.java
@@ -0,0 +1,17 @@
+package com.banking;
+
+public class Simulation {
+	
+	public static void main(String[] args) {
+		/* 
+		 * Create list of clients with accounts that we can "navigate"
+		 * 
+		 * check we can "navigate" accounts (transfer/deposit/withdraw)
+		 * 
+		 * check existing clients can create new accounts
+		 * 
+		 * check new clients can be added 
+		 */
+	}
+
+}
